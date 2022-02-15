fb504991fb082e2d76a10912f621e55d0a28c490
easydevgit <urserg1@gmail.com>
2022-02-15 11:34:53 +0400

    Promise.all() and Promise.allSettled()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function App() {
-  return <div></div>;
+  const [starships, setStarships] = useState([])
+  const [planets, setPlanets] = useState([])
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const resultData = await Promise.allSettled([
+        axios.get(`https://swapi.dev/api/starships4444/`),
+        axios.get(`https://swapi.dev/api/planets/`),
+        axios.get(`https://swapi.dev/api/films/`),
+      ])
+
+      setStarships(resultData[0]?.value)
+      setPlanets(resultData[1]?.value)
+      setFilms(resultData[2]?.value)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <div>
+        {starships?.data?.results.map((item) => (
+          <div key={item.name}>{item.name}</div>
+        ))}
+      </div>
+      <div>
+        {planets?.data?.results.map((item) => (
+          <div key={item.name}>{item.name}</div>
+        ))}
+      </div>
+      <div>
+        {films?.data?.results.map((item) => (
+          <div key={item.title}>{item.title}</div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
